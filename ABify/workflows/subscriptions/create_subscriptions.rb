7c4efb238d812232cd8b6034f812c86549ffb532
Luke <imlukedewitt@gmail.com>
2024-09-12 17:09:37 -0500

    simplify file

--- ABify/workflows/subscriptions/create_subscriptions.rb
+++ ABify/workflows/subscriptions/create_subscriptions.rb
@@ -9,34 +9,48 @@ require_relative '../../models/step'
 class CreateSubscriptions < Workflow
   def unbuilt_steps
     [
-      {
-        name: 'lookup customer',
-        required: true,
-        skip: ->(row) { present?(row['customer id']) },
-        method: :get,
-        url: ->(row, config) { "#{config.base_url}/customers/lookup.json?reference=#{row['customer reference']}" },
-        response_key: 'customer id',
-        response_val: ->(result) { result['customer']['id'] }
-      },
-      {
-        name: 'lookup payment profile',
-        required: true,
-        skip: ->(row) { present?(row['payment profile id']) },
-        method: :get,
-        url: ->(row, config) { "#{config.base_url}/payment_profiles.json?customer_id=#{row['customer id']}" },
-        response_key: 'payment profile id',
-        response_val: ->(result) { result.last['payment_profile']['id'] }
-      },
-      {
-        name: 'create subscription',
-        required: true,
-        method: :post,
-        url: ->(_row, config) { "#{config.base_url}/subscriptions.json" },
-        json: Payloads::Subscriptions.method(:create_subscriptions),
-        response_key: 'subscription id',
-        response_val: ->(result) { result['subscription']['id'] },
-        response_text: ->(result, config) { "#{config.base_url}/subscriptions/#{result['subscription']['id']}" }
-      }
+      lookup_customer_step,
+      lookup_payment_profile_step,
+      create_subscription_step
     ]
+  end
+
+  private
+
+  def lookup_customer_step
+    {
+      name: 'lookup customer',
+      required: true,
+      skip: ->(row) { present?(row['customer id']) },
+      method: :get,
+      url: ->(row, config) { "#{config.base_url}/customers/lookup.json?reference=#{row['customer reference']}" },
+      response_key: 'customer id',
+      response_val: ->(result) { result['customer']['id'] }
+    }
+  end
+
+  def lookup_payment_profile_step
+    {
+      name: 'lookup payment profile',
+      required: true,
+      skip: ->(row) { present?(row['payment profile id']) },
+      method: :get,
+      url: ->(row, config) { "#{config.base_url}/payment_profiles.json?customer_id=#{row['customer id']}" },
+      response_key: 'payment profile id',
+      response_val: ->(result) { result.last['payment_profile']['id'] }
+    }
+  end
+
+  def create_subscription_step
+    {
+      name: 'create subscription',
+      required: true,
+      method: :post,
+      url: ->(_row, config) { "#{config.base_url}/subscriptions.json" },
+      json: Payloads::Subscriptions.method(:create_subscriptions),
+      response_key: 'subscription id',
+      response_val: ->(result) { result['subscription']['id'] },
+      response_text: ->(result, config) { "#{config.base_url}/subscriptions/#{result['subscription']['id']}" }
+    }
   end
 end
