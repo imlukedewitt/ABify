0374743463060e0a51b4d7395e0866ef35e4e93a
Luke <imlukedewitt@gmail.com>
2024-11-12 13:43:54 -0600

    make basic importer controller test

---
+++ ABify/spec/controllers/importer_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../controllers/importer_controller'
+
+RSpec.describe ImporterController do
+  include Rack::Test::Methods
+
+  def app
+    described_class.new
+  end
+
+  it 'responses to requests' do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('It works!')
+  end
+end
