39d3ceab3886c4f68b9f3e8211ddb930e97161f9
Luke <imlukedewitt@gmail.com>
2024-11-11 18:01:50 -0600

    add config spec

---
+++ ABify/spec/models/config_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../models/config'
+
+RSpec.describe Config do
+  subject { described_class.new(api_key: '123', subdomain: 'test') }
+
+  describe '#initialize' do
+    it 'sets the domain to chargify.com by default' do
+      expect(subject.domain).to eq('chargify.com')
+    end
+  end
+
+  describe '#base_url' do
+    it 'returns the base url' do
+      expect(subject.base_url).to eq('https://test.chargify.com')
+    end
+  end
+end
