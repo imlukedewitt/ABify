745e9450eaa3e59969b6af5c1531569c8f00cb03
Luke <imlukedewitt@gmail.com>
2024-11-12 21:42:45 -0600

    unhappy paths

--- ABify/spec/controllers/importer_controller_spec.rb
+++ ABify/spec/controllers/importer_controller_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe ImporterController do
   end
 
   before do
+    allow($stdout).to receive(:write)
     allow(BuildWorkflow).to receive(:for).and_return(instance_double('Workflow'))
     allow(MockData).to receive(:new).and_return(instance_double('MockData'))
     allow(CsvData).to receive(:new).and_return(instance_double('CsvData'))
@@ -47,10 +48,38 @@ RSpec.describe ImporterController do
     post '/start', {}, http_headers
 
     expect(last_response).to be_ok
-
+    expect(last_response.body).to eq({ message: 'started', import_id: '123' }.to_json)
     expect(Importer).to have_received(:new).once
     expect(importer_double).to have_received(:start).once
     expect(MockData).to have_received(:new).once
     expect(BuildWorkflow).to have_received(:for).once
+  end
+
+  it 'returns error for missing row count with mock data' do
+    headers = http_headers.except('HTTP_ROW_COUNT')
+
+    post '/start', {}, headers
+
+    expect(last_response.status).to eq(422)
+    expect(last_response.body).to include('Row count required for mock data')
+  end
+
+  it 'returns error for invalid source type' do
+    headers = http_headers.merge('HTTP_SOURCE_TYPE' => 'invalid')
+
+    post '/start', {}, headers
+
+    expect(last_response.status).to eq(422)
+    expect(last_response.body).to include('Invalid source type')
+  end
+
+  it 'returns error when loading CSV fails' do
+    allow(CsvData).to receive(:new).and_raise(StandardError, 'CSV load error')
+    headers = http_headers.merge('HTTP_SOURCE_TYPE' => 'csv')
+
+    post '/start', {}, headers
+
+    expect(last_response.status).to eq(422)
+    expect(last_response.body).to include('Error loading CSV: CSV load error')
   end
 end
