d5d549bbe11fdcb78cc58d1d49f9d9309018dec5
Luke <imlukedewitt@gmail.com>
2024-11-12 21:34:31 -0600

    move logic to importer controller

--- ABify/controllers/importer_controller.rb
+++ ABify/controllers/importer_controller.rb
@@ -1,13 +1,66 @@
 # frozen_string_literal: true
 
 require 'sinatra/base'
+require_relative '../db/local_keystore'
+require_relative '../models/config'
+require_relative '../models/data_sources/csv_source'
+require_relative '../models/data_sources/json_source'
+require_relative '../models/data_sources/mock_data_source'
+require_relative '../models/importer'
+require_relative '../workflows/build_workflow'
 
 # ImporterController
 class ImporterController < Sinatra::Base
+  keystore = LocalKeystore.instance
+
   get '/' do
-    return 'It works!'
+    'It works!'
   end
 
   post '/start' do
+    puts 'Starting import...'
+
+    config = Config.new(
+      api_key: request.env['HTTP_APIKEY'],
+      subdomain: request.env['HTTP_SUBDOMAIN'],
+      domain: request.env['HTTP_DOMAIN'],
+      keystore: keystore
+    )
+
+    request.body.rewind
+    template_name = request.env['HTTP_TEMPLATE']
+    source_type = request.env['HTTP_SOURCE_TYPE']
+
+    data = case source_type
+           when 'csv'
+             begin
+               CsvData.new(request.env['HTTP_FILE_PATH'])
+             rescue StandardError => e
+               status 422
+               return { error: "Error loading CSV: #{e.message}" }.to_json
+             end
+           when 'json'
+             JsonData.new(request.body.read)
+           when 'mock'
+             row_count = request.env['HTTP_ROW_COUNT']&.to_i
+             unless row_count
+               status 422
+               return { error: 'Row count required for mock data' }.to_json
+             end
+             MockData.new(template_name, row_count)
+           else
+             status 422
+             return { error: 'Invalid source type' }.to_json
+           end
+
+    workflow = BuildWorkflow.for(template_name)
+    importer = Importer.new(config, workflow, data)
+
+    Thread.new do
+      importer.start
+    end
+
+    content_type :json
+    { message: 'started', import_id: importer.id }.to_json
   end
 end
--- ABify/main.rb
+++ ABify/main.rb
@@ -33,53 +33,7 @@ end
 use ImporterController
 
 # keystore = RedisClient.new(ENV['REDIS_URL'], ENV['REDIS_USERNAME'], ENV['REDIS_PASSWORD'])
-keystore = LocalKeystore.new
-
-# placeholder route to start import
-post '/start' do
-  puts 'Starting import...'
-
-  config = Config.new(
-    api_key: request.env['HTTP_APIKEY'],
-    subdomain: request.env['HTTP_SUBDOMAIN'],
-    domain: request.env['HTTP_DOMAIN'],
-    keystore: keystore
-  )
-
-  request.body.rewind
-  template_name = request.env['HTTP_TEMPLATE']
-  source_type = request.env['HTTP_SOURCE_TYPE']
-  data = case source_type
-         when 'csv'
-           begin
-             CsvData.new(request.env['HTTP_FILE_PATH'])
-           rescue StandardError => e
-             status 422
-             return { error: "Error loading CSV: #{e.message}" }.to_json
-           end
-         when 'json'
-           JsonData.new(request.body.read)
-         when 'mock'
-           row_count = request.env['HTTP_ROW_COUNT']&.to_i
-           unless row_count
-             status 422
-             return { error: 'Row count required for mock data' }.to_json
-           end
-           MockData.new(template_name, row_count)
-         else
-           status 422
-           return { error: 'Invalid source type' }.to_json
-         end
-  workflow = BuildWorkflow.for(template_name)
-  importer = Importer.new(config, workflow, data)
-
-  Thread.new do
-    importer.start
-  end
-
-  content_type :json
-  { message: 'started', import_id: importer.id }.to_json
-end
+keystore = LocalKeystore.instance
 
 get '/status' do
   extend Utils
