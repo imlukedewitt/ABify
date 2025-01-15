139fb20a130efc952b9424227c8b425a3b8d061e
Luke <imlukedewitt@gmail.com>
2025-01-15 08:27:57 -0600

    feat: add UI helper functions for rendering HTML and creating menus in Google Sheets

---
+++ Apps-Script/server/helpers/ui.js
@@ -0,0 +1,40 @@
+const UI = (() => {
+  function include(fileName) {
+    return render(fileName).getContent();
+  }
+
+  function render(path) {
+    return htmlBody = HtmlService.createTemplateFromFile(path).evaluate();
+  }
+
+  function renderModal(path, title) {
+    let htmlOutput = render(path);
+    SpreadsheetApp.getUi().showModalDialog(htmlOutput, title);
+  }
+
+  function renderSidebar(path, title) {
+    let htmlOutput = render(path);
+    htmlOutput.setTitle(title);
+    SpreadsheetApp.getUi().showSidebar(htmlOutput);
+  }
+
+  function createMenu() {
+    const ui = SpreadsheetApp.getUi();
+    let menu = ui.createMenu('️🌀 ABify');
+    menu.addItem('Importer...', 'UI.openImportSidebar')
+      .addToUi();
+  }
+
+  function openImportSidebar() {
+    renderSidebar('client/templates/importer-sidebar.html', 'Import Data');
+  }
+
+  return {
+    include,
+    render,
+    renderModal,
+    renderSidebar,
+    createMenu,
+    openImportSidebar
+  }
+})();
