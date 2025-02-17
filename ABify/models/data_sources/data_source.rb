46022099c347bc3dfd26636922227731c0047ae5
Luke <imlukedewitt@gmail.com>
2025-02-17 10:44:25 -0600

    update summary method in DataSource class to include original_data parameter

--- ABify/models/data_sources/data_source.rb
+++ ABify/models/data_sources/data_source.rb
@@ -3,16 +3,14 @@
 # Base class for data sources
 # There can be multiple data sources, each with their own implementation
 class DataSource
-  attr_reader :original_data
-
   # Placeholder method to be handled by subclasses
   def rows
     raise NotImplementedError, 'Subclasses must implement a rows method'
   end
 
-  def summary(data: false)
-    if data
-      @rows.map { |row| row.summary(data: data) }
+  def summary(data: false, original_data: false)
+    if data || original_data
+      @rows.map { |row| row.summary(data: data, original_data: original_data) }
     else
       @rows.select { |row| row.requests.length > 1 }.map { |row| row.summary(data: data) }
     end
