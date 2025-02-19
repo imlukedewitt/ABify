b49fbb735b33cb0835d14270f00c80eb8154e014
Luke <45362944+imlukedewitt@users.noreply.github.com>
2025-02-19 00:06:11 -0600

    [feature] add checkStatus test function (#17)

--- Apps-Script/aaatest.js
+++ Apps-Script/aaatest.js
@@ -17,3 +17,12 @@ function test(siteName, template) {
   importer.sheet.writeImportResults(result.rows);
   Logger.log('great job');
 }
+
+function checkStatus() {
+  const id = '20250219054957-luke-64f31845';
+  const abify = new ABify({
+    creds: Credentials.getCredentialsForSite('luke')
+  });
+  const status = abify.status(id);
+  Logger.log(`Status: ${status.status}, run time: ${status.run_time}`);
+}
