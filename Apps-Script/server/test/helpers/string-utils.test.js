ee56ee900d16a6acd86d0069420c4ace44ae54c9
Luke <imlukedewitt@gmail.com>
2025-02-19 23:00:46 -0600

    use bigger comments

--- Apps-Script/server/test/helpers/string-utils.test.js
+++ Apps-Script/server/test/helpers/string-utils.test.js
@@ -3,12 +3,16 @@ const TestStringUtils = {
     const test = new UnitTestingApp();
     test.enable();
 
-    // LOCAL TESTS
+    /* ====================
+     *     LOCAL TESTS   
+     * ==================== */
     test.runInGas(false);
     test.printHeader('StringUtils local tests');
     test.assert(() => 2 + 2 === 4, '2 + 2 should equal 4');
 
-    // ONLINE TESTS
+    /* ====================
+     *     ONLINE TESTS 
+     * ==================== */
     test.runInGas(true);
     test.printHeader('StringUtils online tests');
     test.assert(() => 3 + 3 === 6, '3 + 3 should equal 6');
