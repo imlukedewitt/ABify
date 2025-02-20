5175033ff23dd4c69dad8013bb6d855bf2fc4aaf
Luke <imlukedewitt@gmail.com>
2025-02-20 00:05:41 -0600

    improve test structure and add some real tests

--- Apps-Script/server/test/helpers/string-utils.test.js
+++ Apps-Script/server/test/helpers/string-utils.test.js
@@ -1,23 +1,93 @@
-const TestStringUtils = {
-  test() {
-    const test = new UnitTestingApp();
-    test.enable();
+const TestStringUtils = (() => {
+  const tester = new UnitTestingApp();
 
+  const blankValues = [null, '', ' ', [], '\n', '\t', '\r'];
+  const nonBlankValues = ['a', 0, 1, {}, [1]];
+  const trueValues = [true, 'true', 'TRUE', ' true ', 'TRUE ', ' true'];
+  const falseValues = [false, 'false', 'FALSE', ' false ', 'FALSE ', ' false'];
+
+  function test() {
     /* ====================
      *     LOCAL TESTS   
      * ==================== */
-    test.runInGas(false);
-    test.printHeader('StringUtils local tests');
-    test.assert(() => 2 + 2 === 4, '2 + 2 should equal 4');
+    tester.runInGas(false);
+    tester.printHeader('StringUtils local tests');
+
+    testsForIsBlank();
+    testsForIsPresent();
+    testsForIsTrue();
+    testsForIsFalse();
+    testsForIsBoolean();
 
     /* ====================
      *     ONLINE TESTS 
      * ==================== */
-    test.runInGas(true);
-    test.printHeader('StringUtils online tests');
-    test.assert(() => 3 + 3 === 6, '3 + 3 should equal 6');
+    tester.runInGas(true);
+    tester.printHeader('StringUtils online tests');
   }
-};
+
+  function testsForIsBlank() {
+    tester.assert(() => {
+      return blankValues.every(StringUtils.isBlank);
+    }, 'isBlank returns true for blank values');
+
+    tester.assert(() => {
+      return nonBlankValues.every(value => !StringUtils.isBlank(value));
+    }, 'isBlank returns false for non-blank values');
+  }
+
+  function testsForIsPresent() {
+    tester.assert(() => {
+      return nonBlankValues.every(StringUtils.isPresent);
+    }, 'isPresent returns true for non-blank values');
+
+    tester.assert(() => {
+      return blankValues.every(value => !StringUtils.isPresent(value));
+    }, 'isPresent returns false for blank values');
+  }
+
+  function testsForIsTrue() {
+    tester.assert(() => {
+      return trueValues.every(StringUtils.isTrue);
+    }, 'isTrue returns true for true values');
+
+    tester.assert(() => {
+      return falseValues.every(value => !StringUtils.isTrue(value));
+    }, 'isTrue returns false for false values');
+  }
+
+  function testsForIsFalse() {
+    tester.assert(() => {
+      return falseValues.every(StringUtils.isFalse);
+    }, 'isFalse returns true for false values');
+
+    tester.assert(() => {
+      return trueValues.every(value => !StringUtils.isFalse(value));
+    }, 'isFalse returns false for true values');
+  }
+
+  function testsForIsBoolean() {
+    tester.assert(() => {
+      return trueValues.every(StringUtils.isBoolean);
+    }, 'isBoolean returns true for true values');
+
+    tester.assert(() => {
+      return falseValues.every(StringUtils.isBoolean);
+    }, 'isBoolean returns true for false values');
+
+    tester.assert(() => {
+      return blankValues.every(value => !StringUtils.isBoolean(value));
+    }, 'isBoolean returns false for blank values');
+
+    tester.assert(() => {
+      return nonBlankValues.every(value => !StringUtils.isBoolean(value));
+    }, 'isBoolean returns false for non-blank non-boolean values');
+  }
+
+  return {
+    test
+  };
+})();
 
 if (typeof module !== "undefined") {
   const UnitTestingApp = require('../unit-testing-app.js');
