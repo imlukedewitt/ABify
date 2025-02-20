dec1177bd8e9a78233bb155384d07c9f4e8d5200
Luke <imlukedewitt@gmail.com>
2025-02-20 00:39:59 -0600

    refactor TestSuite to use an IIFE and separate runServerTests function

--- Apps-Script/server/test/test-suite.js
+++ Apps-Script/server/test/test-suite.js
@@ -1,12 +1,22 @@
-const TestSuite = {
-  run() {
+const TestSuite = (() => {
+  function run() {
+    runServerTests();
+  }
+
+  function runServerTests() {
     const test = new UnitTestingApp();
     test.enable();
     test.clearConsole();
-    
+    console.log('running server tests\n');
+
     TestStringUtils.test();
   }
-};
+
+  return {
+    run,
+    runServerTests
+  };
+})();
 
 if (typeof module !== "undefined") {
   UnitTestingApp = require('./unit-testing-app.js');
